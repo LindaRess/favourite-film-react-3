0dc4dde7c9df139613d68e30180da7196e1fa847
“Linda <linda.resseguier@gmail.com>
2020-11-16 16:37:55 +0100

    new project added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "./components/Form.js";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
